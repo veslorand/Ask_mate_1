6a227f78870ee7539ae0c3fc5a0078b550696527
veslorand <veslorand@gmail.com>
2020-05-26 01:11:04 +0200

    Route fix

--- data_handler.py
+++ data_handler.py
@@ -1,28 +1,42 @@
 import csv
 
 
-def get_answers():
-    try:
-        with open("answer.csv", "r") as file:
-            reader = csv.DictReader(file)
-            return [item for item in reader]
-    except FileNotFoundError:
-        return []
+def get_questions_by_id(id):
+    question_list = []
+    with open("./Desktop/projects/Ask_mate_1/question.csv", "r") as file:
+        reader = csv.reader(file)
+        for row in reader:
+            if id == row[0]:
+                question_list.append(row)
+    return question_list
+
+
+def get_answer_by_id(id):
+    question_list = []
+    with open("./Desktop/projects/Ask_mate_1/question.csv", "r") as file:
+        reader = csv.reader(file)
+        for row in reader:
+            if id == row[0]:
+                question_list.append(row)
+    return question_list
+
 
 def get_all_question():
     try:
-        with open('question.csv') as csv_file:
+        with open("./Desktop/projects/Ask_mate_1/question.csv") as csv_file:
             csv_reader = csv.DictReader(csv_file)
             return [item for item in csv_reader]
     except FileNotFoundError:
         print("except")
         return []
 
+
 def find_question_by_id(id):
     question = get_all_question()
     for dic in question:
         if id in dic:
             return dic
+
 
 def find_answer_by_id(id):
     answer = get_answers()
--- server.py
+++ server.py
@@ -9,7 +9,6 @@ app = Flask(__name__)
 @app.route("/question")
 def list_questions():
     all_question = data_handler.get_all_question()
-
     return render_template("list.html", all_question=all_question)
 
 def find_question():
