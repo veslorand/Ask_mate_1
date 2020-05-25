a0c9f777cca01427ea4508d2c6c401f036503b5f
kunand <kunandras96@gmail.com>
2020-05-25 13:20:58 -0400

    Question by id

--- data_handler.py
+++ data_handler.py
@@ -1,29 +1,35 @@
 import csv
 
-def get_questions():
-    question_list = []
-    with open("question.csv", "r") as file:
-        reader = csv.DictReader(file)
-        for row in reader:
-            question_list.append(row)
-    return question_list
 
 def get_answers():
-    answer_list = []
-    with open("answer.csv", "r") as file:
-        reader = csv.DictReader(file)
-        for row in reader:
-            answer_list.append(row)
-    return answer_list
+    try:
+        with open("answer.csv", "r") as file:
+            reader = csv.DictReader(file)
+            return [item for item in reader]
+    except FileNotFoundError:
+        return []
 
 def get_all_question():
     try:
-        with open('/home/veslorandpc/Desktop/projects/Ask_mate_1/question.csv') as csv_file:
+        with open('question.csv') as csv_file:
             csv_reader = csv.DictReader(csv_file)
             return [item for item in csv_reader]
     except FileNotFoundError:
         print("except")
         return []
 
+def find_question_by_id(id):
+    question = get_all_question()
+    for dic in question:
+        if id in dic:
+            return dic
 
-print(get_all_question())
+def find_answer_by_id(id):
+    answer = get_answers()
+    for dic in answer:
+        if id in dic:
+            return dic
+
+#print(get_answers())
+
+#print(get_all_question())
--- server.py
+++ server.py
@@ -6,11 +6,17 @@ app = Flask(__name__)
 
 @app.route("/")
 @app.route("/list")
+@app.route("/question")
 def list_questions():
     all_question = data_handler.get_all_question()
 
     return render_template("list.html", all_question=all_question)
 
+def find_question():
+    question = data_handler.find_question_by_id(1)
+    answer = data_handler.find_answer_by_id(1)
+
+    return render_template("question.html", question=question, answer=answer)
 
 if __name__ == "__main__":
     app.run(debug=True)
