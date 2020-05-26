baa5d096ad3e020bb79d63ba2212d651644b6587
veslorand <veslorand@gmail.com>
2020-05-26 10:57:32 +0200

    Connection fixed

--- data_handler.py
+++ data_handler.py
@@ -14,24 +14,20 @@ def get_questions_by_id(id, file_name):
             return dic
 
 
-
 def get_all_question():
-#    print(QUESTION_FILE)
-#    try:
-#        with open(QUESTION_FILE) as csv_file:
-#            csv_reader = csv.DictReader(csv_file)
-#            return [item for item in csv_reader]
-#    except FileNotFoundError:
-#        print("except")
-#        return []
+    try:
+        return connection.read_csv_file(QUESTION_FILE)
+    except FileNotFoundError:
+        print("except")
+        return []
 
 
 def find_answer_by_id(id, file_name):
-     answer = connection.read_csv_file(file_name)
-     for dic in answer:
-         if id in dic:
-             return dic
+    answer = connection.read_csv_file(file_name)
+    for dic in answer:
+        if id in dic:
+            return dic
 
-#print(get_answers())
+# print(get_answers())
 
-#print(get_all_question())
+# print(get_all_question())
