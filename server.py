2e763e2a3f5e51a8942799ade2420feddada185a
veslorand <veslorand@gmail.com>
2020-05-26 21:49:45 +0200

    Navbar added and refractor htmls with inheritance

--- server.py
+++ server.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 def list_questions():
     all_question = data_handler.get_all_question()
     sorted_questions_by_date = sorted(all_question, key=lambda i: i['submission_time'])
-    return render_template("list.html", all_question=sorted_questions_by_date, header=data_handler.DATA_HEADER)
+    return render_template("question_list.html", all_question=sorted_questions_by_date, header=data_handler.DATA_HEADER)
 
 
 @app.route('/add_question', methods=['post', 'get'])
@@ -20,8 +20,8 @@ def add_question():
     if request.method == 'POST':
         new_question_data = data_handler.create_question_form(request.form.values())
         connection.append_csv_file(data_handler.QUESTION_FILE, new_question_data, data_handler.QUESTION_HEADER)
-        return redirect("/")
-    return render_template("add_question.html", header=data_handler.DATA_HEADER)
+        return redirect("/question/" + new_question_data[0])
+    return render_template("add_new_question.html", header=data_handler.DATA_HEADER)
 
 
 @app.route("/question/<question_id>")
@@ -29,7 +29,7 @@ def question(question_id):
     line = data_handler.get_questions_by_id(question_id, data_handler.QUESTION_FILE)
     all_answer = data_handler.get_all_answer()
     sorted_answer_by_vote = sorted(all_answer, key=lambda i: i['vote_number'])
-    return render_template("question.html", question=line, header=data_handler.QUESTION_HEADER, all_answer=sorted_answer_by_vote)
+    return render_template("answer_list.html", question=line, header=data_handler.QUESTION_HEADER, all_answer=sorted_answer_by_vote)
 
 
 if __name__ == "__main__":
