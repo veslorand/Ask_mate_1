251c7d99aeda68e5240c2e1451741248d792777e
veslorand <veslorand@gmail.com>
2020-05-28 18:16:15 +0200

    File type validator added

--- data_handler.py
+++ data_handler.py
@@ -9,15 +9,14 @@ QUESTION_FILE = DATA_FOLDER_PATH + "question.csv"
 ANSWER_FILE = DATA_FOLDER_PATH + "answer.csv"
 QUESTIONS_HEADER = ['id', 'submission_time', 'view_number', 'vote_number', 'title', 'message', 'image']
 ANSWERS_HEADER = ['id', 'submission_time', 'vote_number', 'question_id', 'message', 'image']
-
+ALLOWED_EXTENSIONS = {'png', 'jpg'}
 
 def get_questions_by_id(id, file_name):
     question = connection.read_csv_file(file_name)
     for dictionary in question:
-        print(dictionary)
         if id == dictionary['id']:
-            print(dictionary)
             return dictionary
+
 
 def get_answer_by_id(id, file_name):
     answer = connection.read_csv_file(file_name)
@@ -58,9 +57,9 @@ def get_date_time():
     return str(time)
 
 
-def create_question_form(
-        generator):  # 'id', 'submission_time', 'view_number', 'vote_number', 'title', 'message', 'image'
-    my_list = [get_random_id(), get_date_time(), '0', '0', '']
+def create_question_form(generator, filename):  # 'id', 'submission_time', 'view_number', 'vote_number', 'title', 'message', 'image'
+    my_list = [get_random_id(), get_date_time(), '0', '0', filename]
+    print(my_list)
     title_and_message = [i for i in generator]
     for ins in title_and_message[::-1]:
         my_list.insert(4, ins)
@@ -82,7 +81,6 @@ def edit_question(generator, question_id):
             question_by_id['title'] = dictionary[1]
         elif dictionary[0] == "edited_question_message":
             question_by_id['message'] = dictionary[1]
-    print(question_by_id)
     return question_by_id
 
 
@@ -134,3 +132,8 @@ def vote_down_answer(answer_id, file_name):
             else:
                 pass
     return answer_dict
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
